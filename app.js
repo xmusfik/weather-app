70ebda0038a48c7d0b775f893780aca2f1f12ff1
Musfikur rahman <xmusfik@gmail.com>
2019-11-27 00:51:07 +0600

    Second Commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please Provide address')
 } else {
-    geocode(address, (error, {latitude,longitude,location}) => {
+    geocode(address, (error, { latitude, longitude, location }) => {
         if (error) {
             return console.log('Error ', error)
         }
