24d067d126f8ee64a2d219d84e448757257df4dd
Musfikur rahman <xmusfik@gmail.com>
2019-11-27 00:04:35 +0600

    initail commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please Provide address')
+} else {
+    geocode(address, (error, {latitude,longitude,location}) => {
+        if (error) {
+            return console.log('Error ', error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                console.log('Error ', error)
+            }
+            console.log('Location: ', location)
+            console.log(forecastData)
+        })
+    })
+}
